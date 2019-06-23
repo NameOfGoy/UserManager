b87a9f465042b7f6179e0d9ab29338c9198098f3
NameOfGoy <838191453@qq.com>
2019-06-23 21:06:25 +0800

    change mysql config for what server's

--- main/main.go
+++ main/main.go
@@ -4,10 +4,11 @@ import (
     //这里讲db作为go/databases的一个别名，表示数据库连接池
     db "UserManager/database"
     . "UserManager/router"
+    _"github.com/icattlecoder/godaemon"
 )
 func main() {
     //当整个程序完成之后关闭数据库连接
     defer db.Engine.Close()
     router := InitRouter()
     router.Run(":8080")
-}
+}   
